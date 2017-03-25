0851c56edbbe67520fe014d3b93631165eb19db2
Dima Kovalenko <Dima Kovalenko>
2017-03-26 00:34:59 +0300

    верстка №2

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name = "discription" content = "Лендинг"/>
+	<meta name ="keywords" content = "лендинг"/>
+	<title>Лендинг</title>
+	<!--<link rel = "shortcut icon" type="image/x-icon" href = "favicon.png"/>-->
+	<link type="text/css" rel = "stylesheet" href = "css/style.css"/>
+	<link type="text/css" rel = "stylesheet" href = "fancybox/jquery.fancybox.css"/>
+	<link type="text/css" rel = "stylesheet" href = "css/animate.css"/>
+
+	<script type="text/javascript" src="js/jquery-1.12.3.min.js"></script>
+	<script type="text/javascript" src="js/inputmask.js"></script>
+	<script type="text/javascript" src="js/jquery.inputmask.js"></script>
+	<script type="text/javascript" src="js/functions.js"></script>
+	<script type="text/javascript" src="fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src = "js/wow.min.js"></script>
+
+</head>
+<body>
+		<div id = "parent_popup" >
+				<div id = "popup">
+						<div id = "popup_close">
+								<img src = "img/popup_close.png" alt = "Закрыть"/>
+				</div>
+				<div id = "popup_main">
+					<h3>Мы вам перезвоним <br>через несколько минут</h3>
+					<form name = "order" action = "#" method = "post">
+					<div>
+							<input type = "text" name = "phone" placeholder = "Телефон"/>
+						</div>
+						<div>
+						   <input type = "submit" name = "order" value = "Жду звонка">
+						 </div>
+					</form>
+				</div>
+		</div>
+	</div>
+
+	<div id="menu">
+	<a href = "#advantages">Наши преимущества</a>
+	<a href = "#steps">Порядок работы </a>
+	<a href = "#why">Достижение эффективности</a>
+	<a href = "#works">Наши работы и цены</a>
+	<a href = "#guarantee">Гарантия</a>
+	<a class="order" href = "#">Оформить заявку</a>
+	</div>
+	<header>
+		<h1 class = "wow bounceInLeft">Landing Page под ключ</h1>
+		<h2 class = "wow bounceInRight">без предоплаты и с гарантией результата</h2>
+	</header>
+	<div id="advantages">
+		<h2 class = "wow flipInX">Наши преимущества</h2>
+	   <div class = "table">
+	   	<div class="row wow fadeInDown">
+	   	<div class="cell"> 
+	   			<img src = "../landing - ordrer/img/a_1.png"/>
+	   			<p>Срок разработки не более 14 дней</p>
+	   	</div>
+	   		   	<div class="cell"> 
+	   			<img src = "../landing - ordrer/img/a_2.png"/>
+	   			<p>Высокое качество работ (портфолио ниже)</p>
+	   	</div>
+	   		   	<div class="cell"> 
+	   			<img src = "../landing - ordrer/img/a_3.png"/>
+	   			<p>Делаем всё под ключ</p>
+	   	</div>
+	   </div>
+	   	   	<div class="row wow fadeInUp">
+	   	<div class="cell"> 
+	   			<img src = "../landing - ordrer/img/a_4.png"/>
+	   			<p>Бесплатное создание рекламной <br> кампании в Яндекс.Директ <br> (рекламный бюджет уже входит <br> стоимость создания лендинга)</p>
+	   	</div>
+	   		   	<div class="cell"> 
+	   			<img src = "../landing - ordrer/img/a_5.png"/>
+	   			<p>Проводим SPLIT-тестирование <br> для достижения максимальной <br> эффективности лендинга</p>
+	   	</div>
+	   		   	<div class="cell"> 
+	   			<img src = "../landing - ordrer/img/a_6.png"/>
+	   			<p>Без предоплаты!</p>
+	   	</div>
+	   </div>
+	</div>
+	<div class="button wow swing">ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ</div>
+	</div>
+
+	<div id = "steps">
+		<h2 class = "wow flipInX">Порядок работы</h2>
+     <div id = "steps_table">
+     		<div class="step wow slideInLeft">
+     			<p class = "number">01</p>
+     			<p>Вы оставляете заявку</p>
+     		</div>
+     		<div class="step wow slideInLeft">
+     			<p class = "number">03</p>
+     			<p>Мы готовим прототип, в который <br> Вы можете вносить коррективы</p>
+     		</div>
+     		<div class="step wow slideInLeft">
+     			<p class = "number">05</p>
+     			<p>Запускаем рекламу <br> и получаем Вам первых клиентов</p>
+     		</div>
+     		<div class="clear"></div>
+     		<div class="step wow slideInRight">
+     			<p class = "number">02</p>
+     			<p>Мы созваниваемся с Вами <br> и консультируем Вас, опираясь <br> на специфику Вашего бизнеса</p>
+     		</div>
+     		<div class="step wow slideInRight">
+     			<p class = "number">04</p>
+     			<p>Делаем полностью лендинг,<br> в который Вы также можете <br> вносить коррективы</p>
+     		</div>
+     		<div class="step wow slideInRight">
+     			<p class = "number">06</p>
+     			<p>Вы оплачиваете <br> стоимость лендинга, <br> а мы присылаем все <br> исходники, <br> даём доступ <br> к уже работающей <br> рекламной кампании <br> и к первым клиентам</p>
+     		</div>
+     		<div class="clear"></div>
+     </div>
+     <div class="button wow swing">ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ</div>
+</div>
+  <div id="why">
+  	<h2 class = "wow flipInX"> Почему наши лендинги эффективны?</h2>
+  	<div class = "table">
+  	<div class = "row">
+  	<div class="cell wow rotateIn">
+  	<img src = "img/w_1.png"/>
+  	<p>Все наши лендинги оптимизированы под мобильные устройства</p>
+  	</div>
+  	<div class="cell wow rotateIn">
+  	<img src = "img/w_2.png"/>
+  	<p>Мы используем jQuery – библиотеку JavaScript, которая позволяет делать очень приятную анимацию. Это делает Ваш лендинг более красивым и современным, что, безусловно, положительно скажется на конверсии</p>
+  	</div>
+  	<div class="cell wow rotateIn">
+  	<img src = "img/w_3.png"/>
+  	<p>Оповещение о заявках по SMS.<br> По Вашему желанию мы сможем сделать так, чтобы сразу после заявки Вам отправлять SMS с номером телефона клиента. И Вы сразу сможете позвонить ему. Клиент будет точно приятно шокирован от Вашей скорости!</p>
+  	</div>
+  </div>
+	</div>
+  	<div class = "table">
+  	<div class = "row">
+  	<div class="cell wow rotateIn">
+  	<img src = "img/w_4.png"/>
+  	<p>Оптимизация рекламной кампании.<br> Сюда входит анализ каждого ключевого запроса: удаление неэффективных, снижение цен на слабоэффективных и повышение цен на высокоэффективные с целью повышение числа клиентов. В результате, Вы гарантированно получаете максимальное число заявок при минимальной её стоимости</p>
+  	</div>
+   	<div class="cell wow rotateIn">
+  	<img src = "img/w_5.png"/>
+  	<p>В обязательном порядке мы проводим SPLIT-тесты. Например, берётся цена и изменяется в сторону увеличения. Дальше половина клиентов видят повышенную цену, а другая половина – старую цену. И смотрится, от какой половины в итоге больше прибыль. Очень часто получается, что при увеличении цены на 50%, число заявок уменьшается лишь на 10% или не изменяется вовсе. На нашей практике даже бывали случаи, когда количество продаж становится даже больше, поэтому прибыль вырастает сильнее, чем цена.</p>
+  	</div>
+  </div>
+	</div>
+	<div class="button wow swing">ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ</div>
+	</div>
+
+	<div id = "works">
+		<h2 class = "wow flipInX">Наши последние работы и цены на них</h2>    
+		<div class = "table">
+		<div class = "row">
+		 <div class = "cell">
+		 	<div class = "site">
+		 		<a href = "img/works/site_1.jpg">
+		 					<img src = "img/works/site_1_s.png"/>
+		 		</a>
+		 	</div>
+		 	<div class = "table">
+		 		<div class = "row ">
+		 			<div class = "cell">Общая стоимость:</div>
+					<div class = "cell">15000</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Цена заявки:</div>
+					<div class = "cell">190</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Заявок в неделю:</div>
+					<div class = "cell">12</div>
+		 	</div>
+
+		 	</div>
+		 </div>
+
+		 <div class = "cell">
+		 	<div class = "site">
+		 		<a href = "img/works/site_2.jpg">
+		 					<img src = "img/works/site_2_s.png"/>
+		 		</a>
+		 	</div>
+		 	<div class = "table">
+		 		<div class = "row ">
+		 			<div class = "cell">Общая стоимость:</div>
+					<div class = "cell">10000</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Цена заявки:</div>
+					<div class = "cell">280</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Заявок в неделю:</div>
+					<div class = "cell">37</div>
+		 	</div>
+
+		 	</div>
+		 </div>
+
+		 <div class = "cell">
+		 	<div class = "site">
+		 		<a href = "img/works/site_3.jpg">
+		 					<img src = "img/works/site_3_s.png"/>
+		 		</a>
+		 	</div>
+		 	<div class = "table">
+		 		<div class = "row ">
+		 			<div class = "cell">Общая стоимость:</div>
+					<div class = "cell">12000</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Цена заявки:</div>
+					<div class = "cell">160</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Заявок в неделю:</div>
+					<div class = "cell">48</div>
+		 	</div>
+
+		 	</div>
+		 </div>
+
+		</div>
+
+		<div class = "row">
+		 <div class = "cell">
+		 	<div class = "site">
+		 		<a href = "img/works/site_4.jpg">
+		 					<img src = "img/works/site_4_s.png"/>
+		 		</a>
+		 	</div>
+		 	<div class = "table">
+		 		<div class = "row ">
+		 			<div class = "cell">Общая стоимость:</div>
+					<div class = "cell">15000</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Цена заявки:</div>
+					<div class = "cell">240</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Заявок в неделю:</div>
+					<div class = "cell">15</div>
+		 	</div>
+
+		 	</div>
+		 </div>
+
+		 <div class = "cell">
+		 	<div class = "site">
+		 		<a href = "img/works/site_5.jpg">
+		 					<img src = "img/works/site_5_s.png"/>
+		 		</a>
+		 	</div>
+		 	<div class = "table">
+		 		<div class = "row ">
+		 			<div class = "cell">Общая стоимость:</div>
+					<div class = "cell">15000</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Цена заявки:</div>
+					<div class = "cell">220</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Заявок в неделю:</div>
+					<div class = "cell">37</div>
+		 	</div>
+
+		 	</div>
+		 </div>
+
+		 <div class = "cell">
+		 	<div class = "site">
+		 		<a href = "img/works/site_6.jpg">
+		 					<img src = "img/works/site_6_s.png"/>
+		 		</a>
+		 	</div>
+		 	<div class = "table">
+		 		<div class = "row ">
+		 			<div class = "cell">Общая стоимость:</div>
+					<div class = "cell">17000</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Цена заявки:</div>
+					<div class = "cell">350</div>
+		 	</div>
+
+		 			 		<div class = "row ">
+		 			<div class = "cell">Заявок в неделю:</div>
+					<div class = "cell">24</div>
+		 	</div>
+
+		 	</div>
+		 </div>
+
+		</div>
+		</div>
+		</div>
+
+		<div id = "guarantee">
+
+			<div id = "guarantee_text" class = "wow flipInY">
+			<p>Мы гарантируем наивысшее качество своей работы. Но подобные гарантии дают все. Поэтому мы делаем очень просто: не требуем никакой предоплаты, не требуем даже денег на рекламный бюджет. Мы просто всё делаем, а потом показываем результат в виде эффективного лендинга, анализа проведённых SPLIT-тестов, полностью настроенной и оптимизированной рекламной кампании и реальных клиентов. По этой причине мы боимся не получить оплату, поскольку Вам самим будет невыгодно отказаться от всего этого. Однако, если Вам не понравится, то Вы сможете отказаться от лендинга и не платить за него.</p>
+			<p>Согласитесь, что это очень выгодное предложение для Вас, так как Вы не рискуете ни копейкой.</p>
+		</div>
+		<div class="button wow swing">ОСТАВИТЬ ЗАЯВКУ</div>
+		</div>
+		
+		<footer>
+          <div>
+          	<p>Copyright &copy 2016. Все права защищены.    </p>
+          </div>
+          <div>
+          	<p>Телефон: +7 (495) 104-45-54</p>
+          	<p>E-mail: info@mysite.ru</p>
+          </div>
+          <div class = "clear"></div>
+		</footer>
+</body>
+</html>
---
+++ order.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Спасибо за заказ</title>
+	<!--<link rel = "shortcut icon" type="image/x-icon" href = "favicon.png"/>-->
+	<meta name = "discription" content = "Лендинг"/>
+	<meta name ="keywords" content = "лендинг"/>
+	<link type="text/css" rel = "stylesheet" href = "css/order.css"/>
+</head>
+<body>
+	<div>
+		<h1>Спасибо!</h1>
+		<p>Через несколько минут с вами свяжется специалист</p>
+	</div>
+</body>
+</html>
